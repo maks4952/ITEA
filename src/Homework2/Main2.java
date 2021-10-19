90f4b147ad2b2ac728c5eeb73f0a3009d69f41f0
Max <gorihenkomax@gmail.com>
2021-10-19 21:31:59 +0300

    First commit task02

--- src/Homework2/Main2.java
+++ src/Homework2/Main2.java
@@ -11,12 +11,12 @@ public class Main2 {
 		try(BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\Zephyrus\\eclipse-workspace\\JavaAdvance\\src\\Homework2\\file.txt"));){
 			String line;
 			while((line = in.readLine()) != null) {
-				System.out.println(line);
+				System.out.println(line + "task02");
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
 
 	}
